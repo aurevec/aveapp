7ecfc672664404a2c16409b7f1bac059aca1fe3e
Aurelien Vecchiato <aurelien.vecchiato@belighted.com>
2013-12-15 11:29:55 +0100

    add bugsnag

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,3 +1,5 @@
-Bugsnag.configure do |config|
-  config.api_key = "82f393c7d12f303a01241e22110e3f52"
+if defined?(Bugsnag)
+  Bugsnag.configure do |config|
+    config.api_key = "82f393c7d12f303a01241e22110e3f52"
+  end
 end
