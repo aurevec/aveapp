97f46ac823fd07a7b8ec7c0c99fb95fd443e360b
Aurelien Vecchiato <aurelien.vecchiato@belighted.com>
2013-12-22 18:33:38 +0100

    add match decorator

---
+++ app/decorators/match_decorator.rb
@@ -0,0 +1,17 @@
+class MatchDecorator < Draper::Decorator
+
+  delegate_all
+
+  def day_number
+    day.number unless day.blank?
+  end
+
+  def home_team_name
+    home_team.name unless home_team.blank?
+  end
+
+  def away_team_name
+    away_team.name unless away_team.blank?
+  end
+
+end
